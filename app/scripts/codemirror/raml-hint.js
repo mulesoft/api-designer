bd87548b21cd2c19fa9a51005a1336bd53b3ed75
Alberto Pose <albertopose@gmail.com>
2013-07-29 15:48:58 -0700

    Fixing tabs.

--- app/scripts/codemirror/raml-hint.js
+++ app/scripts/codemirror/raml-hint.js
@@ -17,21 +17,21 @@ var a;
     var d = [];
 
     var prevLineText = editor.getLine(cur.line - 1),
-        prevLineTabCount = prevLineText.split('  ').length - 1,
-        currLineTabCount = curLine.split('  ').length - 1;
+      prevLineTabCount = prevLineText.split('  ').length - 1,
+      currLineTabCount = curLine.split('  ').length - 1;
 
     function computePath(editor, tabCount, line) {
-        var tabs = editor.getLine(line).split('  '),
-            value = tabs.pop(), result = [];
+      var tabs = editor.getLine(line).split('  '),
+        value = tabs.pop(), result = [];
 
-        if (tabs.length === tabCount) {
-            if (tabCount !== 0) {
-                result = computePath(editor, tabs.length - 1, line - 1);
-            }
-            return result.concat([value.substring(0, value.length - 1)]);
-        } else {
-            return computePath(editor, tabCount, line - 1);
+      if (tabs.length === tabCount) {
+        if (tabCount !== 0) {
+          result = computePath(editor, tabs.length - 1, line - 1);
         }
+        return result.concat([value.substring(0, value.length - 1)]);
+      } else {
+        return computePath(editor, tabCount, line - 1);
+      }
 
 
     }
@@ -42,41 +42,41 @@ var a;
     console.log(val);
     var s = '  ';
     for (var i = 0; i < currLineTabCount; i++) {
-        s += '  ';
+      s += '  ';
     }
 
     var alternatives = suggest2(val);
     alternativeKeys = Object.keys(alternatives).filter(function (e) {
-        if (e === '<string>') {
-            return false;
-        }
+      if (e === '<string>') {
+        return false;
+      }
 
-        return true;
+      return true;
     });
     list = alternativeKeys.map(function (e) {
 
-        var type = alternatives[e]();
+      var type = alternatives[e]();
 
-        if (typeof type === 'string') {
-            return {text: e + ": ", displayText: e  + ' (autocomplete)'};
+      if (typeof type === 'string') {
+        return {text: e + ": ", displayText: e  + ' (autocomplete)'};
         } else {
             return {text: e + ":\n" + s, displayText: e  + ' (autocomplete)'};
         }
     }).filter(function(e) {
-        if (curWord) {
-            if (e && e.text.indexOf(curWord) === 0) {
-                return true;
-            }
-            return false;
-        }
-        return true;
+      if (curWord) {
+          if (e && e.text.indexOf(curWord) === 0) {
+              return true;
+          }
+          return false;
+      }
+      return true;
     }) || [];
 
     
 
-    return {list: list, 
-        from: CodeMirror.Pos(cur.line, start), 
-          to: CodeMirror.Pos(cur.line, end)
+    return {list: list,
+      from: CodeMirror.Pos(cur.line, start),
+      to: CodeMirror.Pos(cur.line, end)
     };
   });
 })();
