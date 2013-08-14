867a043fbd11c5d80e3fe9a0dbfbf9f03493aa62
Alberto Pose <albertopose@gmail.com>
2013-08-14 10:02:19 -0700

    Oops, forgot App Engine task

--- Gruntfile.js
+++ Gruntfile.js
@@ -350,6 +350,17 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.registerTask('appengine', 'Upload to App Engine.', function() {
+    var spawn = require('child_process').spawn;
+    var PIPE = {stdio: 'inherit'};
+    var done = this.async();
+
+    spawn('appcfg.py', ['update', './dist'], PIPE).on(
+      'exit', function(status) {
+        done(status === 0);
+        }
+      );
+  });
   grunt.registerTask('server', function (target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
