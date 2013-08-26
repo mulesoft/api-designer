a75bc43c5522902948fae7ec56b46f30ee787929
Alberto Pose <albertopose@gmail.com>
2013-08-26 01:51:11 -0300

    Adding more tests for getEditorState and getScope (and some little bugfixing).

--- test/spec/ramlHint.js
+++ test/spec/ramlHint.js
@@ -5,7 +5,7 @@ var hinter, editor;
 var describe = window.describe, beforeEach = window.beforeEach,
     expect = window.expect, it = window.it;
 
-function getEditor(text) {
+function getEditor(text, cursor) {
     var Editor = {
       getCursor: function () {
         return this.cursor;
@@ -22,7 +22,7 @@ function getEditor(text) {
     var editor = Object.create(Editor);
   
     editor.text = text;
-    editor.cursor = 0;
+    editor.cursor = cursor || { line: 0, ch: 0 };
 
     return editor;
 }
@@ -73,14 +73,44 @@ describe('RAML Hint Service', function () {
         '  /bye:\n' +
         '    get: {}\n' +
         '  /ciao:\n' +
-        '    get:\n');
+        '    get:');
       
 
       var scopesByLine = hinter.getScopes(editor).scopesByLine;
-      expect(scopesByLine['0']).not.toBe(undefined);
-      expect(scopesByLine['0'].length).toBe(5);
-      expect(scopesByLine['3'].length).toBe(2);
-      expect(scopesByLine['4'].length).toBe(1);
+      expect(scopesByLine[0]).not.toBe(undefined);
+      expect(scopesByLine[0].length).toBe(4);
+      expect(scopesByLine[3].length).toBe(2);
+      expect(scopesByLine[4].length).toBe(1);
+
+      var scopeLevels = hinter.getScopes(editor).scopeLevels;
+      expect(scopeLevels[0].length).toBe(4);
+      expect(scopeLevels[1].length).toBe(2);
+      expect(scopeLevels[2].length).toBe(2);
+      expect(scopeLevels[3]).toBe(undefined);
+    });
+  });
+
+  describe('getEditorState', function () {
+    it('should be consistent with editor state', function () {
+      editor = getEditor(
+        'title: hello\n'+
+        'version: v1.0\n' +
+        'baseUri: http://example.com/api\n' +
+        '/hello:\n' +
+        '  /bye:\n' +
+        '    get: {}\n' +
+        '  /ciao:\n' +
+        '    get:\n', {line: 4, ch: 4});
+
+      var editorState = hinter.getEditorState(editor);
+      expect(editorState).not.toBe(undefined);
+      expect(editorState.curWord).toBe('bye');
+      expect(editorState.start.line).toBe(4);
+      expect(editorState.start.ch).toBe(3);
+      expect(editorState.end.line).toBe(4);
+      expect(editorState.end.ch).toBe(6);
+      expect(editorState.curLine).toBe('  /bye:');
+      expect(editorState.currLineTabCount).toBe(1);
     });
   });
 
