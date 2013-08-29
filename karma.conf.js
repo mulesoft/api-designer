7d2bca19ea6e0bd935b1b2c86659fb49579fe3dc
Alberto Pose <albertopose@gmail.com>
2013-08-29 19:53:58 -0300

    Changing karma reporting type from 'progress' to 'dots'

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+    reporters: ['dots'],
 
 
     // web server port
