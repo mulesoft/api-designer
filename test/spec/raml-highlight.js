3a88f6b38b4f64bc65ff0702cab9f1e34ef0c19e
Pavel Strashkin <pavel.strashkin@mulesoft.com>
2013-10-16 14:22:17 -0700

    RT-346 Make checking for RAML tag more strict so we do not do wrong highlighting

--- test/spec/raml-highlight.js
+++ test/spec/raml-highlight.js
@@ -1,23 +1,42 @@
 'use strict';
 
-describe('RAML Syntax Highlight', function (){
-
+describe('RAML Syntax Highlight', function () {
   beforeEach(module('codeMirror'));
 
-  describe('getNumberOfIndentTabs', function () {
-    var getNumberOfIndentTabs;
+  describe('codeMirror', function () {
+    describe('getNumberOfIndentTabs', function () {
+      var getNumberOfIndentTabs;
 
-    /* jshint camelcase: false */
-    beforeEach(inject(function (_getNumberOfIndentTabs_) {
-      getNumberOfIndentTabs = _getNumberOfIndentTabs_;
-    }));
-    /* jshint camelcase: true */
+      /* jshint camelcase: false */
+      beforeEach(inject(function (_getNumberOfIndentTabs_) {
+        getNumberOfIndentTabs = _getNumberOfIndentTabs_;
+      }));
+      /* jshint camelcase: true */
 
-    it('should support spaces/tabs after text and not count them ' +
-      'as indent (RT-319)', function() {
+      it('should support spaces/tabs after text and not count them as indent (RT-319)', function () {
+        getNumberOfIndentTabs('    hello:  ').should.be.equal(1);
+      });
+    });
 
-      getNumberOfIndentTabs('    hello:  ').should.be.equal(1);
+    describe('token', function () {
+      /* globals CodeMirror */
+      var token;
 
+      /* jshint camelcase: false */
+      beforeEach(inject(function (_token_) {
+        token = _token_;
+      }));
+      /* jshint camelcase: true */
+
+      it('should mark string, almost identical to a RAML tag, as a comment (RT-346)', function () {
+        var stream = new CodeMirror.StringStream('#%Raml 0.8');
+        token(stream, {}).should.be.equal('comment');
+      });
+
+      it('should mark proper string as a RAML tag (RT-346)', function () {
+        var stream = new CodeMirror.StringStream('#%RAML 0.8');
+        token(stream, {}).should.be.equal('raml-tag');
+      });
     });
   });
 });
