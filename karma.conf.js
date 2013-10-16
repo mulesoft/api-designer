053e9404d494302cd0f5acfebf5ee3065f480b13
Alberto Pose <albertopose@gmail.com>
2013-10-16 16:10:25 -0300

    Adding JUnit reporter for Bamboo agent.

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['dots', 'coverage'],
+    reporters: ['dots', 'coverage', 'junit'],
 
     preprocessors: {
       'app/scripts/**/*.js': ['coverage']
