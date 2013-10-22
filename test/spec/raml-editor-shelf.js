ee77782973d75251a2c2bf067853b064fdb97891
Alberto Pose <albertopose@gmail.com>
2013-10-22 20:11:49 -0300

    Making applySuggestion insert in the cursor or in another line depending on whether the current line is empty or not.

---
+++ test/spec/raml-editor-shelf.js
@@ -0,0 +1,69 @@
+'use strict';
+
+describe('Shelf controller', function () {
+  var applySuggestion;
+
+  beforeEach(module('ramlEditorApp'));
+
+  beforeEach(inject(function (_applySuggestion_) {
+    applySuggestion = _applySuggestion_;
+  }));
+
+  describe('applySuggestion', function () {
+    it('should insert snippet at right place when cursor is in one-key mapping', function () {
+      var editor = getEditor(
+        [
+          'traits:',            //
+          '  - hola:',          //
+          '      displayName:'  // <--
+        ].join('\n'),
+        {
+          line: 2,
+          ch: 6
+        }
+      );
+
+      applySuggestion(editor, {name: 'description'});
+      editor.replaceRangeArguments[0].should.be.equal('      description:\n');
+    });
+
+    it('should insert suggestion in the cursor position on empty line', function () {
+      var editor = getEditor(
+        [
+          'traits:',              //
+          '  - hola:',            //
+          '      displayName:',   //
+          '             '         // <--
+        ].join('\n'),
+        {
+          line: 3,
+          ch: 6
+        }
+      );
+
+      applySuggestion(editor, {name: 'description'});
+      editor.replaceRangeArguments[0].should.be.equal('      description:');
+
+    });
+
+    it('should not insert suggestion in the cursor position on non-empty line', function () {
+      var editor = getEditor(
+        [
+          'traits:',             //
+          '  - hola:',           //
+          '      displayName:'   // <--
+        ].join('\n'),
+        {
+          line: 2,
+          ch: 0
+        }
+      );
+
+      applySuggestion(editor, {name: 'description'});
+      editor.replaceRangeArguments[0].should.be.equal('      description:\n');
+
+    });
+
+  });
+
+});
