36ab9d260920f205f5b8ca8d20e7df26599a4aef
Jazmin Calabrese <jazmin.calabrese@mulesoft.com>
2013-11-05 17:21:16 -0300

    adding test for shelf

--- scenario/test/lib/shelf-helper.js
+++ scenario/test/lib/shelf-helper.js
@@ -2,6 +2,7 @@
 (function() {
 //  var protractor = require('protractor');
   var elementsVersion = ['#%RAML 0.8'];
+//  Root Level
   var elementsRootLevel = ['title','version','schemas','baseUri','mediaType','protocols', 'documentation',
   'baseUriParameters','securitySchemes','securedBy','New resource','traits','resourceTypes'];
   var elementsRootLevelRoot = ['title','version','schemas','baseUri','mediaType','protocols'];
@@ -10,11 +11,27 @@
   var elementsRootLevelSecurity = ['securitySchemes', 'securedBy'];
   var elementsRootLevelResources = ['New resource'];
   var elementsRootLevelTraitsAndTypes = [ 'traits', 'resourceTypes'];
-//  var elementR
+//  Resource level
+  var elementsResourceLevel = ['displayName','get','post','put','delete','head','patch','options','uriParameters','baseUriParameters','securedBy','New resource','is', 'type'];
+  var elementsResourceLevelDocs = ['displayName'];
+  var elementsResourceLevelMethods = ['get','post','put','delete','head','patch','options', 'trace', 'connect'];
+  var elementsResourceLevelParameters = ['uriParameters','baseUriParameters'];
+  var elementsResourceLevelSecurity = ['securedBy'];
+  var elementsResourceLevelResources = ['New resource'];
+  var elementsResourceLevelTraitsAndTypes = ['is', 'type'];
+// Method level
+  var groupsForMethods = 7;
+  var elemMethodLevel = ['protocols','description','baseUriParameters','headers','queryParameters','responses','securedBy','is','body'];
+  var elemMethodLevelRoot = ['protocols'];
+  var elemMethodLevelDocs = ['description'];
+  var elemMethodLevelParameters = ['baseUriParameters','headers','queryParameters'];
+  var elemMethodLevelResponses = ['responses'];
+  var elemMethodLevelSecurity = ['securedBy'];
+  var elemMethodLevelTraitsAndTypes = ['is'];
+  var elemMethodLevelBody = ['body'];
 
 
-  var elementsResourceLevel = ['displayName','get','post','put','delete','head','patch','options','trace','connect','uriParameters','baseUriParameters','securedBy','New resource','is', 'type'];
-
+//Root
 
   global.shelfGetElementsRootLevel = function(){
     return elementsRootLevel;
@@ -37,11 +54,60 @@
   global.shelfGetElementsRootLevelTraitsAndTypes = function(){
     return elementsRootLevelTraitsAndTypes;
   };
-
-
+//  Resource
   global.shelfGetElementsResourceLevel = function(){
     return elementsResourceLevel;
   };
+  global.shelfGetElementsResourceLevelDocs = function(){
+    return elementsResourceLevelDocs;
+  };
+  global.shelfGetElementsResourceLevelMethods = function(){
+    return elementsResourceLevelMethods;
+  };
+  global.shelfGetElementsResourceLevelParameters = function(){
+    return elementsResourceLevelParameters;
+  };
+  global.shelfGetElementsResourceLevelSecurity = function(){
+    return elementsResourceLevelSecurity;
+  };
+  global.shelfGetElementsResourceLevelResources = function(){
+    return elementsResourceLevelResources;
+  };
+  global.shelfGetElementsResourceLevelTraitsAndTypes = function(){
+    return elementsResourceLevelTraitsAndTypes;
+  };
+//  Methods
+  global.shelfGetCantGroupsForMEthods = function(){
+    return groupsForMethods;
+  };
+  global.getelemForMethods = function(){
+    return elemForMethods;
+  };
+  global.shelfGetElemMethodLevel = function(){
+    return elemMethodLevel;
+  };
+  global.shelfGetElemMethodLevelRoot = function(){
+    return elemMethodLevelRoot;
+  };
+  global.shelfGetElemMethodLevelDocs = function(){
+    return elemMethodLevelDocs;
+  };
+  global.shelfGetElemMethodLevelParameters = function(){
+    return elemMethodLevelParameters;
+  };
+  global.shelfGetElemMethodLevelResponses = function(){
+    return elemMethodLevelResponses;
+  };
+  global.shelfGetElemMethodLevelSecurity = function(){
+    return elemMethodLevelSecurity;
+  };
+  global.shelfGetElemMethodLevelTraitsAndTypes = function(){
+    return elemMethodLevelTraitsAndTypes;
+  };
+  global.shelfGetElemMethodLevelBody = function(){
+    return elemMethodLevelBody;
+  };
+
   global.shelfGetElementsVersion = function(){
     return elementsVersion;
   };
@@ -52,4 +118,11 @@
     return browser.findElements(by.css('.'+group+' ul li span'));
   };
 
+  global.ShelfGetSectionsFromShelf = function(){
+    return browser.findElements(by.css('[role=\'section\']'));
+  };
+
+  global.itemsInSection = function(){
+    return '[role=\'items\'] li span';
+  };
 })();
