dc7bbe774f3e07d7079e46296f3bee981a14a172
Jazmin Calabrese <jazmin.calabrese@mulesoft.com>
2013-12-04 16:05:40 -0300

    forget to commit protractor.config.js

--- scenario/support/protractor.conf.js
+++ scenario/support/protractor.conf.js
@@ -6,20 +6,20 @@ exports.config = {
   },
 
   specs: [
-//    '../test/e2e/editor-shelf/resource/resource-methods.js',
-//    '../test/e2e/editor-shelf/resource/resource-root.js',
-//    '../test/e2e/editor-shelf/resource-types/rt-methods.js',
-//    '../test/e2e/editor-shelf/resource-types/rt-root.js',
-//    '../test/e2e/editor-shelf/root.js',
-//    '../test/e2e/editor-shelf/traits.js',
-//    '../test/e2e/editor-parser/*.js',
-//    '../test/e2e/editor-parser/resource/*.js',
-//    '../test/e2e/editor-parser/resourceTypes/*.js',
+    '../test/e2e/editor-shelf/resource/resource-methods.js',
+    '../test/e2e/editor-shelf/resource/resource-root.js',
+    '../test/e2e/editor-shelf/resource-types/rt-methods.js',
+    '../test/e2e/editor-shelf/resource-types/rt-root.js',
+    '../test/e2e/editor-shelf/root.js',
+    '../test/e2e/editor-shelf/traits.js',
+    '../test/e2e/editor-parser/*.js',
+    '../test/e2e/editor-parser/resource/*.js',
+    '../test/e2e/editor-parser/resourceTypes/*.js',
     '../test/e2e/raml-example/muse-e2e.js',
 //    '../test/e2e/editor-console.js',
 //    '../test/e2e/console/console-resource.js',
-//    '../test/e2e/published-examples/examples-parser.js',
-//    '../test/e2e/editor-shelf/shelf-regressions.js',
+    '../test/e2e/published-examples/examples-parser.js',
+    '../test/e2e/editor-shelf/shelf-regressions.js',
     '../test/lib/*.js'
   ],
 //  baseUrl:  'https://ramltooling:ram10ve@j0hnqa.mulesoft.org/',
@@ -39,7 +39,7 @@ exports.config = {
 
   jasmineNodeOpts: {
     onComplete: null,
-    isVerbose: true,
+    isVerbose: false,
     showColors: true,
     includeStackTrace: true,
     defaultTimeoutInterval: 20000
