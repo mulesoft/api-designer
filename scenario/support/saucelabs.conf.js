6a4844740ce6efb23eead67a53fcf49b583683dc
Jazmin Calabrese <jazmin.calabrese@mulesoft.com>
2013-12-09 18:26:04 -0300

    Adding Changes to run in saucelabs

--- scenario/support/saucelabs.conf.js
+++ scenario/support/saucelabs.conf.js
@@ -9,18 +9,18 @@ exports.config = {
   },
 
   specs: [
-    '../test/e2e/editor-shelf/resource/resource-methods.js',
+//    '../test/e2e/editor-shelf/resource/resource-methods.js',
     '../test/e2e/editor-shelf/resource/resource-root.js',
     '../test/e2e/editor-shelf/resource-types/rt-methods.js',
     '../test/e2e/editor-shelf/resource-types/rt-root.js',
     '../test/e2e/editor-shelf/root.js',
     '../test/e2e/editor-shelf/traits.js',
     '../test/e2e/editor-parser/*.js',
-    '../test/e2e/editor-parser/resource/*.js',
-    '../test/e2e/editor-parser/resourceTypes/*.js',
-    '../test/e2e/raml-example/muse-e2e.js',
-    '../test/e2e/published-examples/examples-parser.js',
-    '../test/e2e/editor-shelf/shelf-regressions.js',
+//    '../test/e2e/editor-parser/resource/*.js',
+//    '../test/e2e/editor-parser/resourceTypes/*.js',
+//    '../test/e2e/raml-example/muse-e2e.js',
+//    '../test/e2e/published-examples/examples-parser.js',
+//    '../test/e2e/editor-shelf/shelf-regressions.js',
     '../test/lib/*.js'
   ],
 
