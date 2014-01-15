42a796697f5ac9d1a96444e63d71277e0dc890a7
Pavel Strashkin <pavel.strashkin@mulesoft.com>
2014-01-15 12:31:08 -0800

    Reduce splitter CSS and JS code as we use flex box model

--- app/scripts/directives/splitter.js
+++ app/scripts/directives/splitter.js
@@ -8,26 +8,25 @@ angular.module('splitter', []).directive('ngSplitter', ['$window', 'config',
     var collapsedSize = 20;
 
     /**
-     * @param element
-     * @returns {Object} The element that is in the DOM before the passed-in element
+     * Scales the splitter to the requested size, clipping the size based on
+     * our constraints and toggling the resize chevron if the size of the
+     * next element goes to the minimum value.
+     *
+     * @param splitter Splitter that was moved
+     * @param size Pixels to resize to
      */
-    function getPrevious(element) {
-      element[0].getPreviousMarker = true;
-      var siblings = Array.prototype.slice.call(element.parent().children(), 0);
-      var idx = 0;
-      //Loop through the parent until we find ourselves and return the previous element
-      try {
-        while(idx < siblings.length) {
-          var nextDOMElement = siblings[idx];
-          if (nextDOMElement.getPreviousMarker) {
-            return idx > 0 ? angular.element(siblings[idx - 1]) : null;
-          }
-          idx++;
-        }
-      } finally {
-        delete element.getPreviousMarker;
-      }
-      return null;
+    function resizeNextTo(splitter, preferredSize) {
+      var next      = splitter.next();
+      var size      = Math.max(preferredSize, collapsedSize);
+      var collapsed = size === collapsedSize;
+
+      setChevronState(splitter, collapsed);
+
+      next
+        .css('flex', '0 0 ' + size + 'px')
+        //Hide the scrollbar on collapse
+        .css('overflow', collapsed ? 'hidden' : 'auto')
+      ;
     }
 
     /**
@@ -37,26 +36,9 @@ angular.module('splitter', []).directive('ngSplitter', ['$window', 'config',
      */
     function resizeNextBy(splitter, sizeAttr, delta) {
       var next = splitter.next();
-      if (canResizeNextBy(splitter, sizeAttr, delta)) {
-        var targetSize = getOffsetSize(next, sizeAttr) + delta;
-        resizeNextTo(splitter, targetSize);
-      }
-    }
+      var size = getOffsetSize(next, sizeAttr) + delta;
 
-    /**
-     * @param splitter Splitter that was moved
-     * @param sizeAttr 'width' or 'height'
-     * @param delta Pixels we intend to resize the next element by
-     * @returns {boolean} That the splitter's previous element would
-     * remain larger than 2 x splitter width/height, which is its
-     * minimum size
-     */
-    function canResizeNextBy(splitter, sizeAttr, delta) {
-      var prev = getPrevious(splitter);
-      //Get the original offsetWidth or offsetHeight:
-      var prevElementOffsetSize = getOffsetSize(prev, sizeAttr);
-      //Policy: Previous element cannot shrink to less than 2 x splitter width/height
-      return prevElementOffsetSize - delta >= getOffsetSize(splitter, sizeAttr) * 2;
+      resizeNextTo(splitter, size);
     }
 
     /**
@@ -66,29 +48,12 @@ angular.module('splitter', []).directive('ngSplitter', ['$window', 'config',
      * means collapse chevron is shown
      */
     function setChevronState(splitter, collapsed) {
-      splitter.children().toggleClass('icon-chevron-sign-left', collapsed);
-      splitter.children().toggleClass('icon-chevron-sign-right', !collapsed);
-      saveIsCollapsed(splitter, collapsed);
-    }
+      splitter.children()
+        .toggleClass('icon-chevron-sign-left',   collapsed)
+        .toggleClass('icon-chevron-sign-right', !collapsed)
+      ;
 
-    /**
-     * Scales the splitter to the requested size, clipping the size based on
-     * our constraints and toggling the resize chevron if the size of the
-     * next element goes to the minimum value.
-     *
-     * @param splitter Splitter that was moved
-     * @param size Pixels to resize to
-     */
-    function resizeNextTo(splitter, size) {
-      var next = splitter.next();
-      //Clip the size to a minimum of minSize pixels wide/high
-      size = Math.max(size, collapsedSize);
-      //Show/hide the chevron:
-      var collapsed = size === collapsedSize;
-      setChevronState(splitter, collapsed);
-      next.css('flex', '0 0 ' + size + 'px');
-      //Hide the scrollbar on collapse
-      next.css('overflow', collapsed ? 'hidden' : 'auto');
+      saveIsCollapsed(splitter, collapsed);
     }
 
     /**
@@ -120,19 +85,11 @@ angular.module('splitter', []).directive('ngSplitter', ['$window', 'config',
      * after the splitter
      * @param splitter The splitter element. Should have a unique id.
      * @param sizeAttr 'width' or 'height'
-     * @param applySize Whether the splitter should have the loaded size applied
      * @returns {Number} The splitter size
      */
-    function loadSize(splitter, sizeAttr, applySize) {
+    function loadSize(splitter, sizeAttr) {
       //If no size was saved, use the current size;
-      var size = config.get('splitterSize_' + splitter.attr('id')) || saveSize(splitter, sizeAttr);
-      if (applySize) {
-        resizeNextTo(splitter, size);
-        //In case window is too small for requested splitter size, we resize
-        //the splitter:
-        ensureSplitterFitsInWindow(splitter, sizeAttr, size);
-      }
-      return size;
+      return config.get('splitterSize_' + splitter.attr('id')) || saveSize(splitter, sizeAttr);
     }
 
     /**
@@ -155,32 +112,6 @@ angular.module('splitter', []).directive('ngSplitter', ['$window', 'config',
 
     //endregion
 
-    /**
-     * When window is moved we need to scale the splitter up/down
-     * based on the size of the window and the splitter's preferred
-     * size.
-     * @param splitter The splitter
-     * @param sizeAttr 'width' or 'height'
-     * @param preferredSize Splitter's preferred size
-     */
-    function ensureSplitterFitsInWindow(splitter, sizeAttr, preferredSize) {
-      var prevElementOffsetSize = getOffsetSize(getPrevious(splitter), sizeAttr);
-      //Window shrinks: Ensure that the previous element does not disappear by
-      //shrinking the element after the splitter:
-      var minSize = Math.max(getOffsetSize(splitter, sizeAttr) * 2, 8);
-      while(prevElementOffsetSize < minSize) {
-        resizeNextBy(splitter, sizeAttr, -(minSize - prevElementOffsetSize));
-        prevElementOffsetSize = getOffsetSize(getPrevious(splitter), sizeAttr);
-      }
-
-      //Grow the splitter if it is smaller than its preferred size and is not collapsed
-      if (preferredSize && !loadIsCollapsed(splitter)) {
-        if (getOffsetSize(splitter.next(), sizeAttr) < preferredSize) {
-          resizeNextBy(splitter, sizeAttr, prevElementOffsetSize - minSize);
-        }
-      }
-    }
-
     return {
       restrict: 'A',
       /**
@@ -189,54 +120,62 @@ angular.module('splitter', []).directive('ngSplitter', ['$window', 'config',
        * @param attrs Normalized attribute names/values for the element
        */
       link: function(scope, splitter, attrs) {
-        var isActive  = false;
-        var vertical  = attrs.ngSplitter === 'vertical';
-        var sizeAttr  = vertical ? 'width' : 'height';
-        var mousePos  = vertical ? 'clientX' : 'clientY';
+        var isActive      = false;
+        var vertical      = attrs.ngSplitter === 'vertical';
+        var sizeAttr      = vertical ? 'width' : 'height';
+        var mousePos      = vertical ? 'clientX' : 'clientY';
         var lastPos;
-        //Check the saved splitter state and collapse or resize it
-        var collapsed = loadIsCollapsed(splitter);
-        if (collapsed) {
-          resizeNextTo(splitter, collapsedSize);
-        }
-        //Load the last non-collapsed size the splitter had, and
-        //if the splitter is not collapsed, apply it.
-        var preferredSize = loadSize(splitter, sizeAttr, !collapsed);
+        var collapsed     = loadIsCollapsed(splitter);
+        var preferredSize = loadSize(splitter, sizeAttr);
+        var parent        = splitter.parent();
 
-        //Configure UI events
-        splitter.on('mousedown', function(evt) {
-          //Only respond to left mouse button:
-          if (evt.button === 0) {
-            isActive = true;
-            splitter.parent().addClass('noselect');
-          }
-        }).parent().on('mousemove', function(evt) {
-            if (isActive) {
-              var delta = evt[mousePos] - lastPos;
-              //Scale the elements:
-              resizeNextBy(splitter, sizeAttr, -delta);
-              lastPos = evt[mousePos];
+        resizeNextTo(splitter, collapsed ? collapsedSize : preferredSize);
+
+        // Configure UI events
+        splitter
+          .on('mousedown', function onMouseDown(e) {
+            // Only respond to left mouse button:
+            if (e.button !== 0) {
+              return;
             }
-          }).on('mousedown', function(evt) {
-            lastPos = evt[mousePos];
-          }).on('mouseup', function() {
+
+            isActive = true;
+            lastPos  = e[mousePos];
+
+            parent.addClass('noselect');
+          })
+        ;
+
+        angular.element($window)
+          .on('mousemove', function onMouseMove(e) {
+            if (isActive) {
+              var delta = e[mousePos] - lastPos;
+              // Scale the elements
+              resizeNextBy(splitter, sizeAttr, -delta);
+              lastPos = e[mousePos];
+            }
+          })
+          .on('mouseup', function onMouseUp(e) {
             if (isActive && !loadIsCollapsed(splitter)) {
               preferredSize = saveSize(splitter, sizeAttr);
             }
+
             isActive = false;
-            splitter.parent().removeClass('noselect');
-          });
-        //Wire up the chevron:
-        splitter.children().on('mouseup', function() {
+            lastPos  = e[mousePos];
+
+            parent.removeClass('noselect');
+          })
+        ;
+
+        // Wire up the chevron
+        splitter.children().on('click', function onClick() {
           var isCollapsed = loadIsCollapsed(splitter);
-          var targetSize = isCollapsed ? preferredSize : 20;
+          var targetSize  = isCollapsed ? preferredSize : collapsedSize;
+
           resizeNextTo(splitter, targetSize);
-          ensureSplitterFitsInWindow(splitter, sizeAttr, preferredSize);
+
           return false;
         });
-
-        //Window resizing is treated a bit like a splitter move:
-        angular.element($window).on('resize', function() { ensureSplitterFitsInWindow(splitter, sizeAttr, preferredSize); });
       }
     };
   }
