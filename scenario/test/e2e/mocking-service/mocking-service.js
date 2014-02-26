91bc452811be09ce1289a19cc4d9d60b84001e57
Jazmin Calabrese <jazmin.calabrese@mulesoft.com>
2014-02-26 11:14:55 -0300

    adding test cases for 65580780

--- scenario/test/e2e/mocking-service/mocking-service.js
+++ scenario/test/e2e/mocking-service/mocking-service.js
@@ -7,17 +7,56 @@ var EditorHelper = require('../../lib/editor-helper.js').EditorHelper;
 describe('Mocking service',function(){
 	var editor = new EditorHelper();
 
-	xdescribe('should not displayed mocking service button',function(){
 
+  describe('baseUriParameters defined - mocked uri should add them', function(){
+// currently mocked base uri is ignoring the baseUriParameters - the idea is that those can be added to the new url.
+    it('mocking service is disable by default',function(){
+      var definition = [
+        '#%RAML 0.8',
+        'title: mocking with base Uri',
+        'baseUri: http://myapi.com/{hola}/next/{hola2}',
+        '/rest:'
+      ].join('\\n');
+      editor.setValue(definition);
+      expect(editor.isEnableMockingService()).toEqual('unchecked');
+    });
 
-		xdescribe('non parsable files', function(){
-			xit('raml file without raml version', function (){
+    it('mocking servise button is displayed', function(){
+      expect(editor.isMockingServiceHidden()).toEqual('not hidden');
+    });
 
-			});
+    it('enable mocking-service', function(){
+      editor.enableDisableMockingService().then(function(){
+        expect(editor.isEnableMockingService()).toEqual('checked');
+      });
+    });
 
-		}); // non parsable files
+    it('check that original baseUri was commentout', function(){
+      expect(editor.getLine(3)).toEqual('#baseUri: http://myapi.com/{hola}/next/{hola2}');
 
-	}); // should not displayed mocking service button
+    });
+
+    it('check that new baseUri was added', function(){
+      browser.sleep(5000);
+      expect(editor.getLine(4)).toMatch(/baseUri: http:\/\/ec2-54-235-9-197.compute-1.amazonaws.com:8080\/mocks\/.*\/\{hola\}\/next\/\{hola2\}/);
+
+    });
+
+    it('turn mocking-service off', function(){
+      editor.enableDisableMockingService().then(function(){
+        expect(editor.isEnableMockingService()).toEqual('unchecked');
+      });
+    });
+
+    it('original baseUri should be uncomment', function(){
+      expect(editor.getLine(3)).toEqual('baseUri: http://myapi.com/{hola}/next/{hola2}');
+    });
+
+    it('mocked baseUri should be removed', function(){
+      expect(editor.getLine(4)).toEqual('/rest:');
+    });
+
+  });
 
   describe('with baseUri', function(){
 
@@ -67,37 +106,8 @@ describe('Mocking service',function(){
     });
   });//with baseUri
 
-
-//      pending for new console style to add automated test to check new base uri on try it section
-
-
-
-  xit('baseUriParameters defined - mocked uri should add them', function(){
-// currently mocked base uri is ignoring the baseUriParameters - the idea is that those can be added to the new url.
-
-  });
-
   describe('tryIt section', function(){
-
+//      pending for new console style to add automated test to check new base uri on try it section
   });
-
-  xdescribe('non raml files', function(){
-    xit('yaml',function(){
-
-    });
-    xit('json',function(){
-
-    });
-    xit('xml', function(){
-
-    });
-    xit('xsd', function(){
-
-    });
-    xit('md', function(){
-
-    });
-  }); // non raml files
-
 
 }); //Mocking service
