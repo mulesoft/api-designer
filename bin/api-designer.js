16889e2a68d88b466e595fa4521ef60991cb0c43
Blake Embrey <hello@blakeembrey.com>
2015-06-19 22:45:15 +1000

    Automatically open page when running server

--- bin/api-designer.js
+++ bin/api-designer.js
@@ -2,6 +2,7 @@
 
 var join = require('path').join
 var express = require('express')
+var open = require('open')
 
 var argv = require('yargs')
   .usage('Usage: $0 -p [num]')
@@ -18,7 +19,7 @@ var app = express()
 app.use(express.static(join(__dirname, '../dist')))
 
 app.listen(argv.p, function () {
-  console.log(
-    'API designer running, open http://localhost:' + argv.p + ' to use...'
-  )
+  console.log('API designer running on port ' + argv.p + '...')
+
+  open('http://localhost:' + argv.p)
 })
