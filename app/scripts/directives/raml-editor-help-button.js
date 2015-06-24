08754fe3312d895cdc3109541d52daa024a3332c
Christian Vogel <christianvogel84@gmail.com>
2015-06-24 08:20:54 +0100

    improving code style as suggested with the last PR - extracting CSS, using href, and ordering function parameter by first letter

--- app/scripts/directives/raml-editor-help-button.js
+++ app/scripts/directives/raml-editor-help-button.js
@@ -3,38 +3,34 @@
 
   angular.module('ramlEditorApp')
     .directive('ramlEditorHelpButton', function ramlEditorHelpButton(
-      $window,
+      $modal,
       $timeout,
-      $modal
+      $window
     ) {
       return {
         restrict: 'E',
         templateUrl: 'views/menu/help-menu.tmpl.html',
         link:     function(scope) {
-          scope.contextHelpMenuOpen = false;
+          scope.menuContextHelpOpen = false;
 
           scope.openHelpContextMenu = function () {
             $timeout(function () {
               $window.addEventListener('click', function self () {
                 scope.$apply(function () {
-                  scope.contextHelpMenuOpen = false;
+                  scope.menuContextHelpOpen = false;
                 });
 
                 $window.removeEventListener('click', self);
               });
             });
 
-            scope.contextHelpMenuOpen = true;
+            scope.menuContextHelpOpen = true;
           };
 
           scope.openHelpModal = function openHelpModal() {
             $modal.open({
               templateUrl: 'views/modal/help.html'
             });
-          };
-
-          scope.openLink = function openLink(link) {
-            $window.open(link, '_blank');
           };
         }
       };
